5e21bfce8e8221faf57a543cae979dd49d738bc0
anbya <anbyaalibia@gmail.com>
2021-01-29 09:51:13 +0700

    123

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(cors());
-app.use(bodyParser.json();
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
 app.get("/", function(req,res){
